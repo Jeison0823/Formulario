16ef380059f71ce39d101c50dc35061df091b7ba
Jeison Castellanos <jeison.castellanos08@gmail.com>
2021-10-27 19:10:18 -0500

    esta es mi octava tarea

---
+++ Dom.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByName("nombre"));
+console.log(document.getElementsByClassName("clase"));
+console.log(document.getElementsByTagName("form"));
+console.log(document.getElementsByTagName("input"));
+console.log(document.getElementsByTagName("label"));
+console.log(document.getElementsByTagName("br"));
+console.log(document.getElementsByTagName("script"))
